04fd17120a701021771621c296de8335ea6229a8
Josh Kulp <joshrkulp@gmail.com>
2017-07-26 09:00:57 -0400

    Move to use new type

--- AlexaDiaperSkill/Controllers/AlexaController.cs
+++ AlexaDiaperSkill/Controllers/AlexaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AlexaDiaperSkill.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,28 +10,34 @@ namespace AlexaDiaperSkill.Controllers
   public class AlexaController : ApiController
   {
     [HttpPost, Route("api/alexa/demo")]
-    public dynamic Hello(dynamic request)
+    public AlexaResponse Hello(dynamic request)
     {
-      return new
-      {
-        version = "1.0",
-        sessionAttribute = new { },
-        response = new
-        {
-          outputSpeech = new
-          {
-            type = "PlainText",
-            text = "Hello World"
-          },
-          card = new
-          {
-            type = "Simple",
-            title = "JoshRKulp Hello World",
-            content = "Hello World"
-          },
-          shouldEndSession = true
-        }
-      };
+
+      return new AlexaResponse("Hello World",true);
+      
+      #region Return Dynamic JSON
+      //return new
+      //{
+      //  version = "1.0",
+      //  sessionAttribute = new { },
+      //  response = new
+      //  {
+      //    outputSpeech = new
+      //    {
+      //      type = "PlainText",
+      //      text = "Hello World"
+      //    },
+      //    card = new
+      //    {
+      //      type = "Simple",
+      //      title = "JoshRKulp Hello World",
+      //      content = "Hello World"
+      //    },
+      //    shouldEndSession = true
+      //  }
+      //};
+      #endregion
+
     }
   }
 }
