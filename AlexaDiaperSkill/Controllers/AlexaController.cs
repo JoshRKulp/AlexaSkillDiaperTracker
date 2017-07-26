adfd5725bd7d02e47107605fc93f707fa40c8da3
Josh Kulp <joshrkulp@gmail.com>
2017-07-26 07:06:04 -0400

    First round to get to hello world

---
+++ AlexaDiaperSkill/Controllers/AlexaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AlexaDiaperSkill.Controllers
+{
+  public class AlexaController : ApiController
+  {
+    [HttpPost, Route("api/alexa/demo")]
+    public dynamic Hello(dynamic request)
+    {
+      return new
+      {
+        version = "1.0",
+        sessionAttribute = new { },
+        response = new
+        {
+          outputSpeech = new
+          {
+            type = "PlainText",
+            text = "Hello World"
+          },
+          card = new
+          {
+            type = "Simple",
+            title = "JoshRKulp Hello World",
+            content = "Hello World"
+          },
+          shouldEndSession = true
+        }
+      };
+    }
+  }
+}
